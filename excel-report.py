1c2bee86622b450efa7cb7db49e51439638fcc04
Grace-creator <47767333+grace-creator@users.noreply.github.com>
2023-02-22 11:08:14 -0600

    Update excel-report.py

--- excel-report.py
+++ excel-report.py
@@ -8,21 +8,19 @@ df = pd.read_csv('inventory.csv')
 
 # Find items that are below 5 in quantity and need to be ordered again
 need_order = df[df['quantity'] < 5]
-print('Items that need to be ordered again:')
-print(need_order)
 
 # Find items that have more than 10 in quantity and another group that has over 30 in quantity
 group_10 = df[(df['quantity'] > 10) & (df['quantity'] <= 30)]
 group_30 = df[df['quantity'] > 30]
-print('Items with more than 10 in quantity:')
-print(group_10)
-print('Items with more than 30 in quantity:')
-print(group_30)
+
+# Find the top 10 worst items sold
+worst_items = df.nsmallest(10, 'quantity')
 
 # Create a new Excel workbook and write the results to a new sheet
 with pd.ExcelWriter('inventory_report_reorder.xlsx') as writer:
     need_order.to_excel(writer, sheet_name='Need Order')
     group_10.to_excel(writer, sheet_name='Group 10+')
     group_30.to_excel(writer, sheet_name='Group 30+')
+    worst_items.to_excel(writer, sheet_name='Worst Items Sold')
 
 os.system('start excel.exe inventory_report_reorder.xlsx')
